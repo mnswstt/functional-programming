152f70fa1499fb8ed16f6e0882c1ca19c5b43c2a
rn0z <manutsawee113@gmail.com>
2017-12-30 21:04:27 +0700

    Nested arrows functions example.

---
+++ nested_function.js
@@ -0,0 +1,5 @@
+let dargon = name => size => element => name + ' is a ' + size + ' dargon that breathes ' + element + '!'
+
+let output = dargon('RxZero')('extream large')('fire')
+
+console.log(output)
